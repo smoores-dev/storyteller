b30306fc73ec9eb874020c612dd071b921da5dc2
Shane Friedman <smoores-gpg@friedmans.us>
2023-05-27 19:19:36 -0400

    Working!

--- storyteller/audio.py
+++ storyteller/audio.py
@@ -1,17 +1,23 @@
 from dataclasses import dataclass
+from functools import cache
+import json
+import math
 import shlex
 import subprocess
-from typing import List, cast
+from typing import Callable, List, Union, cast
 import whisperx
+import whisperx.types
 import whisperx.asr
 import os
 from mutagen.mp4 import MP4, Chapter
+from pathlib import PurePath
+from fuzzysearch import Match, find_near_matches
 
 from storyteller.prompt import generate_initial_prompt
 
 
 def get_mp4(book_name: str):
-    return MP4(f"assets/audio/{book_name}.mp4")
+    return MP4(f"assets/audio/{book_name}/original/{book_name}.mp4")
 
 
 @dataclass
@@ -22,15 +28,17 @@ class ChapterRange:
 
 
 def get_chapter_filename(book_filename: str, chapter_title: str):
-    filename, ext = os.path.splitext(book_filename)
-    return f"{filename}-{chapter_title}{ext}"
+    path, basename = os.path.split(book_filename)
+    filename, ext = os.path.splitext(basename)
+    chapters_path = PurePath(path, "..", "chapters")
+    return f"{chapters_path}/{filename}-{chapter_title}{ext}"
 
 
-def split_audiobook(book_name: str):
+def split_audiobook(book_name: str) -> List[str]:
     mp4 = get_mp4(book_name)
     filename = cast(str, mp4.filename)
     if mp4.chapters is None:
-        return
+        return []
 
     chapters: List[Chapter] = list(mp4.chapters)
     chapter_ranges: List[ChapterRange] = []
@@ -40,20 +48,38 @@ def split_audiobook(book_name: str):
         )
         chapter_ranges.append(ChapterRange(chapter, chapter.start, next_chapter_start))
 
+    chapter_filenames: List[str] = []
     for range in chapter_ranges:
+        chapter_filename = get_chapter_filename(filename, range.chapter.title)
+        chapter_filenames.append(chapter_filename)
         command = shlex.split(
-            f'ffmpeg -nostdin -ss {range.start} -to {range.end} -i "{mp4.filename}" -c copy -map 0 -map_chapters -1 "{get_chapter_filename(filename, range.chapter.title)}"'
+            f'ffmpeg -nostdin -ss {range.start} -to {range.end} -i "{mp4.filename}" -c copy -map 0 -map_chapters -1 "{chapter_filename}"'
         )
-        subprocess.run(command)
+        devnull = open(os.devnull, "w")
+        subprocess.run(command, stdout=devnull, stderr=devnull)
+
+    return chapter_filenames
 
 
-def transcribe_chapter(filename: str, chapter_text: str):
-    initial_prompt = generate_initial_prompt(chapter_text)
+def transcribe_chapter(filename: str):
+    chapter_path, chapter_basename = os.path.split(filename)
+    chapter_name, _ = os.path.splitext(chapter_basename)
+    transcription_filename = PurePath(
+        chapter_path, "..", "transcriptions", f"{chapter_name}.json"
+    )
+
+    if os.path.exists(transcription_filename):
+        with open(transcription_filename, mode="r") as transcription_file:
+            transcription = json.load(transcription_file)
+            return cast(whisperx.types.AlignedTranscriptionResult, transcription)
+
+    # initial_prompt = generate_initial_prompt(chapter_text)
     model = whisperx.load_model(
         "base.en",
         device="cpu",
         compute_type="int8",
-        asr_options={"word_timestamps": True, "initial_prompt": initial_prompt},
+        asr_options={"word_timestamps": True},
+        # asr_options={"word_timestamps": True, "initial_prompt": initial_prompt},
     )
 
     audio = whisperx.load_audio(filename)
@@ -63,5 +89,149 @@ def transcribe_chapter(filename: str, chapter_text: str):
     alignment_model, metadata = whisperx.load_align_model(
         language_code=unaligned["language"], device="cpu"
     )
-    transcription = whisperx.align(unaligned["segments"], alignment_model, metadata, audio, device="cpu", return_char_alignments=False)  # type: ignore
+    transcription = whisperx.align(
+        unaligned["segments"],  # type: ignore
+        alignment_model,
+        metadata,
+        audio,
+        device="cpu",
+        return_char_alignments=False,
+    )
+
+    with open(transcription_filename, mode="w") as transcription_file:
+        json.dump(transcription, transcription_file)
+
     return transcription
+
+
+def get_transcription_text(transcription: whisperx.types.AlignedTranscriptionResult):
+    return "\uc3a9".join([segment["text"] for segment in transcription["segments"]])
+
+
+def find_timestamps(match: Match, transcription):
+    s = 0
+    position = 0
+    while True:
+        while position + len(transcription["segments"][s]["text"]) < match.start:  # type: ignore
+            position += len(transcription["segments"][s]["text"]) + 1  # type: ignore
+            s += 1
+
+        w = 0
+        segment = transcription["segments"][s]
+        while (
+            w < len(segment["words"])
+            and position + len(segment["words"][w]["word"]) <= match.start
+        ):
+            position += len(segment["words"][w]["word"]) + 1
+            w += 1
+        if w >= len(segment["words"]):
+            s += 1
+            continue
+
+        break
+
+    start_word = segment["words"][w]
+
+    # If a segment only has one word, the start and
+    # end timestamps are only placed on the segment
+    if "start" in start_word:
+        return start_word["start"]
+
+    return segment["start"]
+
+
+@dataclass
+class SentenceRange:
+    start: float
+    end: float
+    id: int
+
+
+def clean_up_sentence_ranges(
+    ranges: List[SentenceRange], duration: float, num_sentences: int
+) -> List[SentenceRange]:
+    sorted_ranges = sorted(ranges, key=lambda r: r.start)
+
+    runs: List[List[SentenceRange]] = [[]]
+    for sentence_range in sorted_ranges:
+        if len(runs[-1]) == 0:
+            runs[-1].append(sentence_range)
+            continue
+
+        if 0 < sentence_range.id - runs[-1][-1].id <= 2:
+            runs[-1].append(sentence_range)
+            continue
+
+        runs.extend([[sentence_range], []])
+
+    valid_ranges = [
+        sentence_range
+        for run in runs
+        if len(run) > 1
+        for sentence_range in run
+        if sentence_range.id != 0
+    ]
+
+    final_ranges: List[Union[SentenceRange, None]] = [None] * num_sentences
+
+    for sentence_range in valid_ranges:
+        final_ranges[sentence_range.id] = sentence_range
+
+    final_ranges[0] = SentenceRange(0, 0, 0)
+
+    for current_range in valid_ranges:
+        index = current_range.id
+        next_range_index = index + 1
+        while (
+            next_range_index < len(final_ranges)
+            and final_ranges[next_range_index] is None
+        ):
+            next_range_index += 1
+
+        next_range: SentenceRange = (
+            final_ranges[next_range_index]
+            if next_range_index < len(final_ranges)
+            else SentenceRange(duration, duration, next_range_index)
+        )  # type: ignore
+
+        gap = next_range.id - current_range.id
+        start_diff = next_range.start - current_range.start
+        for i in range(1, gap):
+            start = current_range.start + start_diff * i / gap
+            final_ranges[index + i] = SentenceRange(start, start, current_range.id + i)
+
+        current_range.end = (
+            final_ranges[index + 1].start if index + 1 < len(final_ranges) else duration   # type: ignore
+        )
+        index += 1
+
+    return cast(List[SentenceRange], final_ranges)
+
+
+def get_chapter_timestamps(
+    transcription: whisperx.types.AlignedTranscriptionResult,
+    sentences: List[str],
+    duration: float,
+):
+    sentence_ranges: List[SentenceRange] = []
+    transcription_text = get_transcription_text(transcription).lower()
+    for index, sentence in enumerate(sentences):
+        matches = find_near_matches(
+            sentence.strip().lower(),
+            transcription_text,
+            max_l_dist=math.floor(0.25 * len(sentence)),
+        )
+        matches = cast(List[Match], matches)
+        if len(matches) == 0:
+            continue
+        first_match = matches[0]
+        if first_match.matched.startswith("\uc3a9"):
+            first_match = Match(
+                first_match.start + 1, first_match.end, first_match.dist, matched=first_match.matched[1:]  # type: ignore
+            )
+        start = find_timestamps(first_match, transcription)
+        sentence_ranges.append(SentenceRange(start, start, index))
+
+    cleaned_sentence_ranges = clean_up_sentence_ranges(sentence_ranges, duration, len(sentences))
+
+    return cleaned_sentence_ranges
--- storyteller/epub.py
+++ storyteller/epub.py
@@ -1,110 +1,68 @@
 import math
 import os
 import re
-import zipfile
+import whisperx.types
 from dataclasses import dataclass
 from typing import Callable, List, cast, Dict
-from fuzzysearch import Match, find_near_matches
+
+# from fuzzysearch import Match
 from nltk.tokenize import sent_tokenize as _sent_tokenize
 from functools import cache
 from ebooklib import epub, ITEM_DOCUMENT
 from bs4 import BeautifulSoup, NavigableString, ResultSet, Tag
+from mutagen.mp4 import MP4
 
-from storyteller.audio import transcribe_chapter
+from storyteller.audio import SentenceRange, get_chapter_timestamps
 
 
 sent_tokenize: Callable[[str], List[str]] = cache(_sent_tokenize)
 
 
-def get_chapters(book_name: str):
-    book = epub.read_epub(f"/workspaces/storyteller/assets/text/{book_name}.epub")
-    chapters: List[epub.EpubHtml] = [
-        chapter
-        for chapter in book.get_items()
-        if chapter.get_type() == ITEM_DOCUMENT and chapter.is_chapter()
-    ]
+def read_epub(book_name: str):
+    book = epub.read_epub(f"assets/text/{book_name}/original/{book_name}.epub")
+    for item in book.get_items_of_type(ITEM_DOCUMENT):
+        if not item.is_chapter():
+            continue
+        soup = BeautifulSoup(item.content)
+
+        head: Tag | None = soup.find("head")  # type: ignore
+        if head is not None:
+            links = head.find_all("link")
+            for link in links:
+                item.add_link(
+                    href=link["href"], rel=" ".join(link["rel"]), type=link["type"]
+                )
+    return book
+
+
+def get_chapters(book: epub.EpubBook) -> List[epub.EpubHtml]:
+    spine_ids = [item[0] for item in book.spine]
+    chapters = [cast(epub.EpubHtml, book.get_item_with_id(id)) for id in spine_ids]
     return chapters
 
 
 endswithalpha = re.compile(".*[a-zA-Z0-9]$")
+consecutivenewlines = re.compile("[\n ]+")
 
 
+@cache
 def get_chapter_text(chapter: epub.EpubHtml):
     soup = BeautifulSoup(chapter.get_body_content(), "html.parser")
-    # This is goofy, but we want nltk to treat separate textblocks as separate
-    # sentences
+    return re.sub(consecutivenewlines, " ", soup.get_text())
+
+
+@cache
+def get_chapter_sentences(chapter: epub.EpubHtml):
+    soup = BeautifulSoup(chapter.get_body_content(), "html.parser")
     textblocks: ResultSet[Tag] = soup.find_all(
         ["h1", "h2", "h3", "h4", "h5", "h6", "p"]
     )
-    for textblock in textblocks:
-        text = textblock.get_text()
-        if endswithalpha.match(text.strip()):
-            textblock.append(".")
-        break
-    text = soup.get_text(" ")
-    return text
 
-
-def find_timestamps(match: Match, transcription):
-    s = 0
-    position = 0
-    while position + len(transcription["segments"][s]["text"]) < match.start:  # type: ignore
-        position += len(transcription["segments"][s]["text"]) + 1  # type: ignore
-        s += 1
-    w = 0
-    segment = transcription["segments"][s]
-    while position + len(segment["words"][w]["word"]) <= match.start:
-        position += len(segment["words"][w]["word"]) + 1
-        w += 1
-
-    start_word = segment["words"][w]
-    start = start_word["start"]
-
-    while position + len(transcription["segments"][s]["text"]) < match.end:  # type: ignore
-        position += len(transcription["segments"][s]["text"]) + 1  # type: ignore
-        s += 1
-        w = 0
-
-    segment = transcription["segments"][s]
-    while (
-        w + 1 < len(segment["words"]) - 1
-        and position + len(segment["words"][w]["word"]) < match.end
-    ):
-        position += len(segment["words"][w]["word"]) + 1
-        w += 1
-
-    end_word = segment["words"][w]
-    end = end_word["end"]
-    return start, end
-
-
-@dataclass
-class SentenceRange:
-    start: float
-    end: float
-    sentence_number: int
-
-
-def get_chapter_timestamps(transcription, chapter_text: str):
-    sentences = sent_tokenize(chapter_text)
-    sentence_timestamps: List[SentenceRange] = []
-    transcription_text = " ".join(
-        [segment["text"].lower() for segment in transcription["segments"]]
-    )
-    for index, sentence in enumerate(sentences):
-        matches = find_near_matches(
-            sentence.strip().lower(),
-            transcription_text,
-            max_l_dist=math.floor(0.2 * len(sentence)),
-        )
-        matches = cast(List[Match], matches)
-        if len(matches) == 0:
-            print(f"no match for '{sentence}'")
-            continue
-        first_match = matches[0]
-        start, end = find_timestamps(first_match, transcription)
-        sentence_timestamps.append(SentenceRange(start, end, index))
-    return sentence_timestamps
+    return [
+        re.sub(consecutivenewlines, " ", sentence)
+        for textblock in textblocks
+        for sentence in sent_tokenize(textblock.get_text())
+    ]
 
 
 @dataclass
@@ -227,9 +185,11 @@ def create_media_overlay(
     seq["epub:textref"] = f"../{chapter_filename}"
     seq["epub:type"] = "chapter"
     soup.body.append(seq)  # type: ignore
-    for id, sentence_range in enumerate(timestamps):
-        par = soup.new_tag("par", id=f"sentence{id}")
-        text = soup.new_tag("text", src=f"../{chapter_filename}#sentence{id}")
+    for sentence_range in timestamps:
+        par = soup.new_tag("par", id=f"sentence{sentence_range.id}")
+        text = soup.new_tag(
+            "text", src=f"../{chapter_filename}#sentence{sentence_range.id}"
+        )
         audio = soup.new_tag(
             "audio",
             src=f"../{audio_filename}",
@@ -250,22 +210,29 @@ class SyncedChapter:
     duration: float
 
 
-def process_chapter(audio_filename: str, chapter: epub.EpubHtml):
-    chapter_text = get_chapter_text(chapter)
-    transcription = transcribe_chapter(audio_filename, chapter_text)
-    timestamps = get_chapter_timestamps(transcription, chapter_text)
+def sync_chapter(
+    mp4: MP4,
+    transcription: whisperx.types.AlignedTranscriptionResult,
+    chapter: epub.EpubHtml,
+):
+    chapter_sentences = get_chapter_sentences(chapter)
+    timestamps = get_chapter_timestamps(
+        transcription, chapter_sentences, mp4.info.length
+    )
     tag_sentences(chapter)
+    chapter_filepath_length = len(chapter.file_name.split(os.path.sep)) - 1
+    relative_ups = "../" * chapter_filepath_length
     chapter.add_link(
-        # TODO: We can't hard-code the relative URL to the styles
-        # here; we need to inspect the paths to figure it out
-        rel="stylesheet", href="../Styles/storyteller-readaloud.css", type="text/css"
+        rel="stylesheet",
+        href=f"{relative_ups}Styles/storyteller-readaloud.css",
+        type="text/css",
     )
     base_filename, _ = os.path.splitext(os.path.basename(chapter.file_name))
-    _, audio_ext = os.path.splitext(audio_filename)
+    _, audio_ext = os.path.splitext(mp4.filename)  # type: ignore
     audio_item = epub.EpubItem(
         uid=f"{base_filename}_audio",
         file_name=f"Audio/{base_filename}{audio_ext}",
-        content=open(audio_filename, "rb").read(),
+        content=open(mp4.filename, "rb").read(),  # type: ignore
         media_type="audio/mpeg",
     )
     media_overlay_item = epub.EpubSMIL(
@@ -280,7 +247,7 @@ def process_chapter(audio_filename: str, chapter: epub.EpubHtml):
         chapter=chapter,
         audio=audio_item,
         media_overlay=media_overlay_item,
-        duration=timestamps[-1].end,
+        duration=timestamps[-1].end if len(timestamps) else 0,
     )
 
 
@@ -291,42 +258,44 @@ def format_duration(duration: float):
     return f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{round(seconds, 3)}"
 
 
-if __name__ == "__main__":
-    book = epub.read_epub(f"/workspaces/storyteller/assets/text/tress.epub")
-    chapter_one: epub.EpubHtml = list(book.get_items())[9]
-    synced = process_chapter(
-        "assets/audio/tress-Chapter 01 - The Girl.mp4", chapter_one
-    )
-    book.add_metadata(
-        None, "meta", format_duration(synced.duration), {"property": "media:duration"}
-    )
+def update_synced_chapter(book: epub.EpubBook, synced: SyncedChapter):
     book.add_metadata(
         None,
         "meta",
         format_duration(synced.duration),
         {"property": "media:duration", "refines": f"#{synced.media_overlay.id}"},
     )
-    book.add_metadata(
-        None, "meta", "-epub-media-overlay-active", {"property": "media:active-class"}
-    )
-    book.add_item(
-        epub.EpubItem(
-            uid="storyteller_readaloud_styles",
-            file_name="Styles/storyteller-readaloud.css",
-            media_type="text/css",
-            content=".-epub-media-overlay-active { background-color: #ffb; }".encode(),
-        )
-    )
+
     book.add_item(synced.audio)
     book.add_item(synced.media_overlay)
-    epub.write_epub("assets/text/tress-test1.epub", book)
 
-    # Annoying hack to prevent overwriting the existing head
-    original_soup = BeautifulSoup(chapter_one.content, "html.parser")
-    with zipfile.ZipFile("assets/text/tress-test1.epub") as epub_archive:
-        epub_archive.filelist
-        with epub_archive.open(f"EPUB/{chapter_one.file_name}", mode="r") as chapter_to_read:
-            written_soup = BeautifulSoup(chapter_to_read.read(), "html.parser")
-            written_soup.extend(original_soup.contents)
-        with epub_archive.open(f"EPUB/{chapter_one.file_name}", mode="w") as chapter_to_write:
-            chapter_to_write.write(written_soup.encode(formatter="html"))
+
+# if __name__ == "__main__":
+# book = read_epub("tress")
+# chapter_one: epub.EpubHtml = list(book.get_items())[9]
+# synced = sync_chapter(
+#     "assets/audio/tress/chapters/tress-Chapter 01 - The Girl.mp4", chapter_one
+# )
+# book.add_metadata(
+#     None, "meta", format_duration(synced.duration), {"property": "media:duration"}
+# )
+# book.add_metadata(
+#     None,
+#     "meta",
+#     format_duration(synced.duration),
+#     {"property": "media:duration", "refines": f"#{synced.media_overlay.id}"},
+# )
+# book.add_metadata(
+#     None, "meta", "-epub-media-overlay-active", {"property": "media:active-class"}
+# )
+# book.add_item(
+#     epub.EpubItem(
+#         uid="storyteller_readaloud_styles",
+#         file_name="Styles/storyteller-readaloud.css",
+#         media_type="text/css",
+#         content=".-epub-media-overlay-active { background-color: #ffb; }".encode(),
+#     )
+# )
+# book.add_item(synced.audio)
+# book.add_item(synced.media_overlay)
+# epub.write_epub("assets/text/tress-test1.epub", book)
