54d927d22351645b4ee5097bc2a09f07976f7b25
Shane Friedman <smoores-gpg@friedmans.us>
2023-06-09 02:27:25 +0000

    Add basic support for configuring server host

--- storyteller/web/routes/index.tsx
+++ storyteller/web/routes/index.tsx
@@ -1,14 +1,21 @@
 import { Head } from "$fresh/runtime.ts";
 import BookUpload from "../islands/BookUpload.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    return await ctx.render(Deno.env.get("STORYTELLER_SERVER_HOST"));
+  },
+};
+
+export default function Home({ data: serverHost }: PageProps<string>) {
   return (
     <>
       <Head>
         <title>Storyteller</title>
       </Head>
       <main>
-        <BookUpload />
+        <BookUpload serverHost={serverHost} />
       </main>
     </>
   );
