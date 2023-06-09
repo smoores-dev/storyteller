31bb57ca1f9b79d2569f7b16e33db015c8f235f4
Shane Friedman <smoores-gpg@friedmans.us>
2023-06-09 00:11:48 -0400

    Listen on 0.0.0.0

--- storyteller/web/main.ts
+++ storyteller/web/main.ts
@@ -9,4 +9,4 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-await start(manifest);
+await start(manifest, { port: 8000, hostname: "0.0.0.0" });
