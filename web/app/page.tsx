39fd215f08ec9457e38151cd9199d9f2f733640b
Shane Friedman <smoores-gpg@friedmans.us>
2023-06-17 04:31:28 +0000

    Fix lint errors

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,12 +6,12 @@ export default async function Home() {
   const booksResponse = await fetch(`${apiHost}/books`)
   const books = await booksResponse.json()
   return (
-    <main className={styles.main}>
+    <main className={styles["main"]}>
       <h2>Your books</h2>
       <AddBookModal apiHost={apiHost} />
       <ul>
         {books.map((book) => (
-          <li>
+          <li key={book.id}>
             <div>{book.title}</div>
             <div>by {book.authors[0].name}</div>
             {book.processing_status && (
