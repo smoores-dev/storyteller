b6c16e97cb25421b76f27020a04014812e98c9a6
Shane Friedman <smoores-gpg@friedmans.us>
2023-10-02 18:59:10 -0400

    Fix some links

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,51 +1,57 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-import Link from '@docusaurus/Link';
+import React from "react"
+import clsx from "clsx"
+import styles from "./styles.module.css"
+import Link from "@docusaurus/Link"
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<"svg">>
+  description: JSX.Element
+}
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Sync your audiobooks and ebooks',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Sync your audiobooks and ebooks",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Storyteller is a platform for automatically syncing audiobooks and ebooks.
+        Storyteller is a platform for automatically syncing audiobooks and
+        ebooks.
       </>
     ),
   },
   {
-    title: 'Read or listen however you choose',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Read or listen however you choose",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Storyteller produces EPUB 3 compliant ebook files. You can read them with any
-        ebook reader (software or hardware!) that supports EPUB Media Overlays, or you
-        can use the dedicated <Link to="/docs/reading-your-books/storyteller-apps">Storyteller mobile apps</Link>.
+        Storyteller produces EPUB 3 compliant ebook files. You can read them
+        with any ebook reader (software or hardware!) that supports EPUB Media
+        Overlays, or you can use the dedicated{" "}
+        <Link to="/docs/category/reading-your-books/storyteller-apps">
+          Storyteller mobile apps
+        </Link>
+        .
       </>
     ),
   },
   {
-    title: 'Own your books',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Own your books",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Storyteller is completely <Link to="/docs/getting-started">self-hosted</Link>.
-        All of your books stay on your hardware, and you're free to move, copy, and
-        back them up as needed.
+        Storyteller is completely{" "}
+        <Link to="/docs/getting-started">self-hosted</Link>. All of your books
+        stay on your hardware, and you're free to move, copy, and back them up
+        as needed.
       </>
     ),
   },
-];
+]
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -54,7 +60,7 @@ function Feature({title, Svg, description}: FeatureItem) {
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures(): JSX.Element {
@@ -68,5 +74,5 @@ export default function HomepageFeatures(): JSX.Element {
         </div>
       </div>
     </section>
-  );
+  )
 }
