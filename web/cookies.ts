8053208c7f9bc39b4e46ee7add15056baff06a86
Shane Friedman <smoores-gpg@friedmans.us>
2023-12-24 11:51:06 -0500

    Only set Secure cookie attribute when requests are made over https

--- web/cookies.ts
+++ web/cookies.ts
@@ -6,3 +6,12 @@ export function getCookieDomain(origin: string | null) {
   const url = new URL(origin)
   return url.hostname
 }
+
+export function getCookieSecure(origin: string | null) {
+  if (origin === null) {
+    return false
+  }
+
+  const url = new URL(origin)
+  return url.protocol === "https:"
+}
