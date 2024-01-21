e55dcb709742a469d256f220e04c53510c6c275d
Shane Friedman <smoores-gpg@friedmans.us>
2024-01-20 19:06:40 -0500

    Fix next.config broken from merge

--- web/next.config.js
+++ web/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  basePath: ROOT_PATH,
-  async rewrites() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: `${API_HOST}/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
