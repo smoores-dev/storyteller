bbcd9519535d8f4cc44f5facfc13f2e26916bc93
Shane Friedman <smoores-gpg@friedmans.us>
2024-04-14 02:31:47 +0000

    Add some tests for determining remaining tasks

---
+++ web/work/__tests__/worker.test.ts
@@ -0,0 +1,102 @@
+import { ProcessingTask } from "@/database/processingTasks"
+import { describe, it } from "node:test"
+import { determineRemainingTasks } from "../worker"
+import { randomUUID } from "node:crypto"
+import assert from "node:assert"
+import {
+  ProcessingTaskStatus,
+  ProcessingTaskType,
+} from "@/apiModels/models/ProcessingStatus"
+
+describe("determineRemainingTasks", () => {
+  it("produces all tasks when none exist", () => {
+    const input: ProcessingTask[] = []
+
+    const uuid = randomUUID()
+    const output = determineRemainingTasks(uuid, input)
+
+    assert.deepStrictEqual(output, [
+      {
+        type: ProcessingTaskType.SPLIT_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+      {
+        type: ProcessingTaskType.TRANSCRIBE_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+      {
+        type: ProcessingTaskType.SYNC_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+    ])
+  })
+
+  it("only produces non-completed tasks when some are completed", () => {
+    const uuid = randomUUID()
+
+    const input: ProcessingTask[] = [
+      {
+        uuid: randomUUID(),
+        type: ProcessingTaskType.SPLIT_CHAPTERS,
+        status: ProcessingTaskStatus.COMPLETED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+    ]
+
+    const output = determineRemainingTasks(uuid, input)
+
+    assert.deepStrictEqual(output, [
+      {
+        type: ProcessingTaskType.TRANSCRIBE_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+      {
+        type: ProcessingTaskType.SYNC_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+    ])
+  })
+
+  it("only produces non-completed tasks when some are completed and some are not", () => {
+    const uuid = randomUUID()
+
+    const input: ProcessingTask[] = [
+      {
+        uuid: randomUUID(),
+        type: ProcessingTaskType.SPLIT_CHAPTERS,
+        status: ProcessingTaskStatus.COMPLETED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+      {
+        uuid: randomUUID(),
+        type: ProcessingTaskType.TRANSCRIBE_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+      {
+        uuid: randomUUID(),
+        type: ProcessingTaskType.SYNC_CHAPTERS,
+        status: ProcessingTaskStatus.STARTED,
+        progress: 0,
+        bookUuid: uuid,
+      },
+    ]
+
+    const output = determineRemainingTasks(uuid, input)
+
+    assert.deepStrictEqual(output, [input[1], input[2]])
+  })
+})
