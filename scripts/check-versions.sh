16dab177281b267c622ca67419c549639656dc45
Shane Friedman <smoores-gpg@friedmans.us>
2024-05-06 03:20:38 +0000

    v0.3.1

--- scripts/check-versions.sh
+++ scripts/check-versions.sh
@@ -2,7 +2,7 @@
 
 VERSION_CHECK=$(yarn version check)
 
-if git diff origin/main -- ./package.json | grep -q -e '^+ *"version":' || echo $VERSION_CHECK | grep -q -v -e '@storyteller/web'
+if git diff origin/main -- web/package.json | grep -q -e '^+ *"version":' || echo $VERSION_CHECK | grep -q -v -e '@storyteller/web'
 then
   exit 0
 else
@@ -10,7 +10,7 @@ else
   exit 1
 fi
 
-if git diff origin/main -- ./package.json | grep -q -e '^+ *"version":' || echo $VERSION_CHECK | grep -q -v -e '@storyteller/docs'
+if git diff origin/main -- docs/package.json | grep -q -e '^+ *"version":' || echo $VERSION_CHECK | grep -q -v -e '@storyteller/docs'
 then
   exit 0
 else
