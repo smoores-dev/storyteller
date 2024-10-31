7316c35214c9a9756162a6b4e31892e7c62661a7
Shane Friedman <smoores-gpg@friedmans.us>
2024-10-31 02:21:45 +0000

    Persistent queue
    Make the BookEvents emitter and processing queue/controllers cache/piscina singletons actually singletons, working around a Next.js app directory bug that can result in a single module being executed multiple times by stashing instances on `globalThis`.
    
    Changelog: fixed

--- web/events.ts
+++ web/events.ts
@@ -16,9 +16,38 @@ export type BookDetailMessage =
 
 export type BookEvent = BookProcessingEvent | BookDetailMessage
 
-export const BookEvents = new EventEmitter<{
+/**
+ * Next.js app directory seems to have a bug where, in production,
+ * a single module can be imported multiple times (breaking the module
+ * cache) if it's depended on by different modules that end up in different
+ * bundled chunks.
+ *
+ * This results in multiple instances of the module level values in this
+ * module, all of which rely on being singletons to work correctly.
+ */
+declare global {
+  // variables declared with const/let cannot be added to the global scope
+  /* eslint-disable no-var */
+  var BookEvents:
+    | EventEmitter<{
+        message: [BookEvent]
+      }>
+    | undefined
+  /* eslint-enable no-var */
+}
+
+export let BookEvents: EventEmitter<{
   message: [BookEvent]
-}>()
+}>
+
+if (globalThis.BookEvents) {
+  BookEvents = globalThis.BookEvents
+} else {
+  BookEvents = new EventEmitter<{
+    message: [BookEvent]
+  }>()
+  globalThis.BookEvents = BookEvents
+}
 
 export function subscribeToBookEvents(listener: (event: BookEvent) => void) {
   BookEvents.on("message", listener)
