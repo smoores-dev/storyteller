7a7f3e6099ab70cf38bcbfa932ccaff49b4ea7df
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-02 11:19:29 -0500

    Switch to native node stream to avoid web stream GC bug

--- web/fs.ts
+++ web/fs.ts
@@ -7,14 +7,17 @@ import { open } from "node:fs/promises"
  */
 export async function streamFile(path: string): Promise<Uint8Array> {
   const fileHandle = await open(path)
-  const stats = await fileHandle.stat()
-  const fileData = new Uint8Array(stats.size)
-  let i = 0
-  for await (const chunk of fileHandle.readableWebStream()) {
-    const chunkArray = new Uint8Array(chunk as ArrayBuffer)
-    fileData.set(chunkArray, i)
-    i += chunkArray.byteLength
+  try {
+    const stats = await fileHandle.stat()
+    const fileData = new Uint8Array(stats.size)
+    let i = 0
+    for await (const chunk of fileHandle.createReadStream()) {
+      const chunkArray = new Uint8Array(chunk as ArrayBuffer)
+      fileData.set(chunkArray, i)
+      i += chunkArray.byteLength
+    }
+    return fileData
+  } finally {
+    await fileHandle.close()
   }
-  await fileHandle.close()
-  return fileData
 }
