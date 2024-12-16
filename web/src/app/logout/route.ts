fffc977d61af2f6c906638352ce56833d952f549
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-16 16:45:38 +0000

    Fix logout redirect -- use origin header instead of request url
    Fixes #175.
    
    Changelog: fixed

--- web/src/app/logout/route.ts
+++ web/src/app/logout/route.ts
@@ -1,9 +1,17 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 import { createAuthedApiClient } from "@/authedApiClient"
+import { headers } from "next/headers"
 
 export const dynamic = "force-dynamic"
-export async function GET(request: NextRequest) {
+export async function GET() {
   const client = await createAuthedApiClient()
   await client.logout()
-  return NextResponse.redirect(new URL("/login", request.url))
+
+  const requestOrigin = (await headers()).get("Origin")
+
+  if (!requestOrigin) {
+    return NextResponse.json({ message: "Logged out" })
+  }
+
+  return NextResponse.redirect(new URL("/login", requestOrigin))
 }
