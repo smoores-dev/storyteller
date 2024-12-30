c6437e6fce5a6dc8b1a785bde6a74fc7cba90361
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-30 00:15:55 -0500

    Handle missing cookie on server

--- web/src/authedApiClient.ts
+++ web/src/authedApiClient.ts
@@ -5,10 +5,9 @@ import { logger } from "./logging"
 
 export async function createAuthedApiClient() {
   const cookieStore = await cookies()
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const authTokenCookie = cookieStore.get("st_token")!
+  const authTokenCookie = cookieStore.get("st_token")
 
-  return new ApiClient(apiHost, proxyRootPath, authTokenCookie.value)
+  return new ApiClient(apiHost, proxyRootPath, authTokenCookie?.value)
 }
 
 export async function getCurrentUser() {
