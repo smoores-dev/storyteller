aada7ee91de53bdb5490bd057ddeb3f2280301f3
Shane Friedman <smoores-gpg@friedmans.us>
2025-07-27 14:40:27 -0400

    web-v2.0.0-15

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from "@/apiClient"
 import { NextRequest, NextResponse } from "next/server"
 import { apiHost, proxyRootPath } from "./app/apiHost"
-import { cookies } from "next/headers"
+import { nextAuth } from "./auth/auth"
 
 export async function middleware(request: NextRequest) {
   const isInitPage = request.nextUrl.pathname.startsWith("/init")
@@ -21,14 +21,13 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
-  const cookieStore = await cookies()
-  const authTokenCookie = cookieStore.get("st_token")
+  const session = await nextAuth.auth()
   const isLoginPage = request.nextUrl.pathname.startsWith("/login")
-  if (!authTokenCookie && !isLoginPage) {
+  if (!session && !isLoginPage) {
     return NextResponse.redirect(new URL("/login", request.url))
   }
 
-  if (authTokenCookie && isLoginPage) {
+  if (session && isLoginPage) {
     return NextResponse.redirect(new URL("/", request.url))
   }
 
@@ -36,5 +35,16 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login", "/settings", "/users", "/init"],
+  runtime: "nodejs",
+  matcher: [
+    "/",
+    "/books",
+    "/books/:uuid",
+    "/collections/:uuid",
+    "/login",
+    "/settings",
+    "/users",
+    "/account",
+    "/init",
+  ],
 }
