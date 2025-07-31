02557b9862959db5d90828157a6a6fc7256e5fe3
Shane Friedman <smoores-gpg@friedmans.us>
2025-07-31 11:03:39 -0400

    web-v2.0.0-52

--- web/src/app/api/v2/books/upload/[[...path]]/route.ts
+++ web/src/app/api/v2/books/upload/[[...path]]/route.ts
@@ -25,6 +25,9 @@ import {
 import { persistAudio, persistEpub } from "@/assets/fs"
 import { getDefaultStatus } from "@/database/statuses"
 import { mkdir, rename } from "node:fs/promises"
+import { AsyncSemaphore } from "@esfx/async-semaphore"
+
+const mutex = new AsyncSemaphore(1)
 
 const server = new Server({
   path: "/api/v2/books/upload",
@@ -39,181 +42,188 @@ const server = new Server({
       }
     }
 
-    const bookUuid = upload.metadata["bookUuid"] as UUID | undefined
-    if (!bookUuid) {
-      return { status_code: 405, body: "Missing required metadata: bookUuid" }
-    }
+    await mutex.wait()
 
-    const filename = upload.metadata["filename"]
-    if (!filename) {
-      return { status_code: 405, body: "Missing required metadata: filename" }
-    }
-
-    const filetype = upload.metadata["filetype"] ?? lookup(filename)
-
-    const collectionUuid = upload.metadata["collection"] as UUID | undefined
-
-    const isEpub = filetype !== false && filetype.startsWith("application/epub")
-    const isAudiobook = isAudioFile(filename)
-    if (!isEpub && !isAudiobook) {
-      return {
-        status_code: 405,
-        body: "Invalid upload type. Expected application/epub, audio/*, or video/mp4.",
+    try {
+      const bookUuid = upload.metadata["bookUuid"] as UUID | undefined
+      if (!bookUuid) {
+        return { status_code: 405, body: "Missing required metadata: bookUuid" }
       }
-    }
 
-    // This hook is only called when storage has been successfully set
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const uploadPath = upload.storage!.path
-    if (isEpub) {
-      const epub = await Epub.from(uploadPath)
-      const manifest = await epub.getManifest()
-      const isAligned = Object.values(manifest).some(
-        (item) => item.mediaOverlay,
-      )
+      const filename = upload.metadata["filename"]
+      if (!filename) {
+        return { status_code: 405, body: "Missing required metadata: filename" }
+      }
 
-      let book = await getBook(bookUuid)
-      if (book) {
-        let update: BookUpdate | null = null
-        const series: SeriesRelation[] = []
+      const filetype = upload.metadata["filetype"] ?? lookup(filename)
 
-        const title = await epub.getTitle()
-        if (title) {
-          update ??= {}
-          update.title = title
+      const collectionUuid = upload.metadata["collection"] as UUID | undefined
+
+      const isEpub =
+        filetype !== false && filetype.startsWith("application/epub")
+      const isAudiobook = isAudioFile(filename)
+      if (!isEpub && !isAudiobook) {
+        return {
+          status_code: 405,
+          body: "Invalid upload type. Expected application/epub, audio/*, or video/mp4.",
         }
+      }
 
-        const publicationDate = await epub.getPublicationDate()
-        if (publicationDate) {
-          update ??= {}
-          update.publicationDate = publicationDate.toISOString()
+      // This hook is only called when storage has been successfully set
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const uploadPath = upload.storage!.path
+      if (isEpub) {
+        const epub = await Epub.from(uploadPath)
+        const manifest = await epub.getManifest()
+        const isAligned = Object.values(manifest).some(
+          (item) => item.mediaOverlay,
+        )
+
+        let book = await getBook(bookUuid)
+        if (book) {
+          let update: BookUpdate | null = null
+          const series: SeriesRelation[] = []
+
+          const title = await epub.getTitle()
+          if (title) {
+            update ??= {}
+            update.title = title
+          }
+
+          const publicationDate = await epub.getPublicationDate()
+          if (publicationDate) {
+            update ??= {}
+            update.publicationDate = publicationDate.toISOString()
+          }
+
+          const description = await epub.getDescription()
+          if (description) {
+            update ??= {}
+            update.description = description
+          }
+
+          const metadata = await epub.getMetadata()
+
+          for (const entry of metadata) {
+            if (
+              entry.properties["property"] === "belongs-to-collection" &&
+              entry.value
+            ) {
+              const typeEntry = metadata.find(
+                (e) =>
+                  e.properties["refines"] === `#${entry.id}` &&
+                  entry.properties["property"] === "collection-type",
+              )?.value
+
+              if (typeEntry !== "series") continue
+
+              const position = metadata.find(
+                (e) =>
+                  e.properties["refines"] === `#${entry.id}` &&
+                  entry.properties["property"] === "group-position",
+              )?.value
+
+              series.push({
+                name: entry.value,
+                featured: true,
+                ...(position && { position: parseFloat(position) }),
+              })
+            }
+
+            if (
+              entry.properties["property"] === "storyteller:version" &&
+              entry.value
+            ) {
+              update ??= {}
+              update.alignedByStorytellerVersion = entry.value
+            }
+
+            if (
+              entry.properties["property"] ===
+                "storyteller:media-overlays-modified" &&
+              entry.value
+            ) {
+              update ??= {}
+              update.alignedAt = entry.value
+            }
+          }
+
+          const updated = await updateBook(book.uuid, update, {
+            series,
+            ...(isAligned
+              ? {
+                  readaloud: {
+                    status: "ALIGNED",
+                    filepath: getInternalEpubAlignedFilepath(book),
+                  },
+                }
+              : { ebook: { filepath: getInternalEpubFilepath(book) } }),
+          })
+
+          await persistEpub(book, uploadPath, isAligned)
+
+          // If the audio was uploaded/processed first, it's going to
+          // potentially have an arbitrary book directory name. Better
+          // to use the one from the ebook
+          await rename(
+            getInternalBookDirectory(book),
+            getInternalBookDirectory(updated),
+          )
+        } else {
+          book = await createBookFromEpub(
+            epub,
+            { uuid: bookUuid, title: basename(filename, ".epub") },
+            {
+              ...(collectionUuid && { collections: [collectionUuid] }),
+            },
+          )
+
+          await persistEpub(book, uploadPath, isAligned)
         }
+      }
 
-        const description = await epub.getDescription()
-        if (description) {
-          update ??= {}
-          update.description = description
-        }
+      if (isAudiobook) {
+        const relativePath =
+          upload.metadata["relativePath"] === "null" ||
+          !upload.metadata["relativePath"]
+            ? filename
+            : upload.metadata["relativePath"]
 
-        const metadata = await epub.getMetadata()
+        let book = await getBook(bookUuid)
 
-        for (const entry of metadata) {
-          if (
-            entry.properties["property"] === "belongs-to-collection" &&
-            entry.value
-          ) {
-            const typeEntry = metadata.find(
-              (e) =>
-                e.properties["refines"] === `#${entry.id}` &&
-                entry.properties["property"] === "collection-type",
-            )?.value
+        if (book) {
+          const filepath = getInternalOriginalAudioFilepath(book, relativePath)
 
-            if (typeEntry !== "series") continue
+          await mkdir(dirname(filepath), { recursive: true })
 
-            const position = metadata.find(
-              (e) =>
-                e.properties["refines"] === `#${entry.id}` &&
-                entry.properties["property"] === "group-position",
-            )?.value
-
-            series.push({
-              name: entry.value,
-              featured: true,
-              ...(position && { position: parseFloat(position) }),
+          if (!book.audiobook) {
+            book = await updateBook(bookUuid, null, {
+              audiobook: {
+                filepath: getInternalOriginalAudioFilepath(book),
+              },
             })
           }
 
-          if (
-            entry.properties["property"] === "storyteller:version" &&
-            entry.value
-          ) {
-            update ??= {}
-            update.alignedByStorytellerVersion = entry.value
-          }
+          await rename(uploadPath, filepath)
+        } else {
+          const defaultStatus = await getDefaultStatus()
 
-          if (
-            entry.properties["property"] ===
-              "storyteller:media-overlays-modified" &&
-            entry.value
-          ) {
-            update ??= {}
-            update.alignedAt = entry.value
-          }
-        }
-
-        const updated = await updateBook(book.uuid, update, {
-          series,
-          ...(isAligned
-            ? {
-                readaloud: {
-                  status: "ALIGNED",
-                  filepath: getInternalEpubAlignedFilepath(book),
-                },
-              }
-            : { ebook: { filepath: getInternalEpubFilepath(book) } }),
-        })
-
-        await persistEpub(book, uploadPath, isAligned)
-
-        // If the audio was uploaded/processed first, it's going to
-        // potentially have an arbitrary book directory name. Better
-        // to use the one from the ebook
-        await rename(
-          getInternalBookDirectory(book),
-          getInternalBookDirectory(updated),
-        )
-      } else {
-        book = await createBookFromEpub(
-          epub,
-          { uuid: bookUuid, title: basename(filename, ".epub") },
-          {
-            ...(collectionUuid && { collections: [collectionUuid] }),
-          },
-        )
-
-        await persistEpub(book, uploadPath, isAligned)
-      }
-    }
-
-    if (isAudiobook) {
-      const relativePath =
-        upload.metadata["relativePath"] === "null" ||
-        !upload.metadata["relativePath"]
-          ? filename
-          : upload.metadata["relativePath"]
-
-      let book = await getBook(bookUuid)
-
-      if (book) {
-        const filepath = getInternalOriginalAudioFilepath(book, relativePath)
-
-        await mkdir(dirname(filepath), { recursive: true })
-
-        if (!book.audiobook) {
-          book = await updateBook(bookUuid, null, {
-            audiobook: {
-              filepath: getInternalOriginalAudioFilepath(book),
+          book = await createBook(
+            {
+              uuid: bookUuid,
+              title: basename(filename, extname(filename)),
+              statusUuid: defaultStatus.uuid,
             },
-          })
+            { ...(collectionUuid && { collections: [collectionUuid] }) },
+          )
+
+          await persistAudio(book, uploadPath, relativePath)
         }
-
-        await rename(uploadPath, filepath)
-      } else {
-        const defaultStatus = await getDefaultStatus()
-
-        book = await createBook(
-          {
-            uuid: bookUuid,
-            title: basename(filename, extname(filename)),
-            statusUuid: defaultStatus.uuid,
-          },
-          { ...(collectionUuid && { collections: [collectionUuid] }) },
-        )
-
-        await persistAudio(book, uploadPath, relativePath)
       }
+      return {}
+    } finally {
+      mutex.release()
     }
-    return {}
   },
 })
 
