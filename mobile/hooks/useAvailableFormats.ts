922e48a065fda4fbccc07a8d04bca82492222bc0
Rachel S <root@rachel.systems>
2026-01-02 03:55:44 +0000

    Only allow readaloud download when aligned
    Prevents the readaloud download button from being visible when a readaloud hasn't finished aligning
    
    Changelog-mobile: fixed

--- mobile/hooks/useAvailableFormats.ts
+++ mobile/hooks/useAvailableFormats.ts
@@ -5,13 +5,21 @@ import { type BookWithRelations } from "@/database/books"
 export function useAvailableFormats(
   book: BookWithRelations | undefined | null,
 ) {
-  return useMemo(
-    () =>
-      book
-        ? [book.readaloud, book.ebook, book.audiobook]
-            .filter((format) => !!format)
-            .map((format) => format.format)
-        : [],
-    [book],
-  )
+  return useMemo(() => {
+    const possibleFormats: (
+      | BookWithRelations["ebook"]
+      | BookWithRelations["audiobook"]
+      | BookWithRelations["readaloud"]
+      | undefined
+    )[] = [book?.ebook, book?.audiobook]
+
+    if (book?.readaloud?.status === "ALIGNED") {
+      possibleFormats.push(book?.readaloud)
+    }
+    return book
+      ? possibleFormats
+          .filter((format) => !!format)
+          .map((format) => format.format)
+      : []
+  }, [book])
 }
