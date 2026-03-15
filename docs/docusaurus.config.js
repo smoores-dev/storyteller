35fda1c157481b76f0a9e165877b259f91ca0743
Shane Friedman <smoores-gpg@friedmans.us>
2026-03-15 14:37:13 -0400

    docs-v2.6.3

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://storyteller-platform.gitlab.com.io/",
+  url: "https://storyteller-platform.dev/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/storyteller/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
